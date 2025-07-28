e04a52adced67aec71b13a8c7d43b4cb3a4ed34b
shailesh89116 <shailesh.gehlot.sg@gmail.com>
2025-07-28 10:16:07 +0530

    added changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { FeaturedProducts } from "@/components/featured-products";
 import { HowItWorks } from "@/components/how-it-works";
 import { PoweredByACL } from "@/components/powered-by-acl";
 import { ProductCategories } from "@/components/products-category";
-import { VideoShowcase } from "@/components/video-showcase";
+// import { VideoShowcase } from "@/components/video-showcase";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
       <ProductCategories />
       <FeaturedProducts />
       <BenefitsSection />
-      <VideoShowcase/>
+      {/* <VideoShowcase/> */}
       <HowItWorks />
     </div>
   );
